2230fe26012ccfa94ad027651d5277ca42811a0a
Zerrari <1660822219@qq.com>
2021-05-09 09:45:40 +0800

    Chapter6

---
+++ Chapter5/operatorDemo2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int x = 2;
+    int y = 4;
+    int a = -x*y;
+    int b = (-x)*y;
+    cout << a << '\n' << b << '\n';
+    cout << 1+2,2+3;
+    return 0;
+}
